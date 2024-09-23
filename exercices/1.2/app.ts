77bcc57ffe9425119b58109d115a8cbcf3f616fa
ali-elhannouti-vinci <ali.elhannouti@student.vinci.be>
2024-09-24 01:38:53 +0200

    1.2 start

--- exercices/1.2/app.ts
+++ exercices/1.2/app.ts
@@ -7,6 +7,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+let getCounter:number = 0;
+app.use((_req,_res,next)=> {
+    console.log(`Request received: ${_req.method} ${_req.url}`);
+    if (_req.method == "GET") {
+        getCounter+=1;
+        
+    }
+    console.log(
+        `GET counter : ${getCounter}`
+    );
+    next();
+});
+
 app.use("/films",filmsRouter);
+
 
 export default app;
