dd85bf28a6bf9fe9851ecf9d4e9341e3819fa978
ali-elhannouti-vinci <ali.elhannouti@student.vinci.be>
2024-10-30 17:28:17 +0100

    2.5

--- exercices/2.5/src/Components/ClickCounter/ClickCounter.tsx
+++ exercices/2.5/src/Components/ClickCounter/ClickCounter.tsx
@@ -1,11 +1,34 @@
 import { useState } from "react";
 
-const ClickCounter = () => {
+interface CounterProps{
+    title:string,
+    message:string,
+    hoverMessage:string
+}
+
+const ClickCounter = ({title,message,hoverMessage}:CounterProps) => {
   const [count, setCount] = useState(0);
+  const [isMouseOver,setIsMouseOver] = useState(false)
 
   const handleClick = () => {
     setCount((count) => count + 1);
-  };
-  return <button onClick={handleClick}>count is {count}</button>;
-};
+  }
+
+  const handleMouseOver = () => {
+    setIsMouseOver(true);
+  }
+
+  const handleMouseLeave = () => {
+    setIsMouseOver(false);
+  }
+
+
+  return <div>
+    <h1>{title}</h1>
+    <div>{isMouseOver ? hoverMessage : <br />}</div>
+    <button onClick={handleClick} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+        count is {count}</button>
+    <h1>{count >= 10 ? message : <p><br /></p>}</h1>
+    </div>;
+}
 export default ClickCounter;
