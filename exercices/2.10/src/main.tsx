f2d29a2995666db12de9d17ec8274a7123f2179b
ali-elhannouti-vinci <ali.elhannouti@student.vinci.be>
2024-11-13 17:26:08 +0100

    2.10 start

--- exercices/2.10/src/main.tsx
+++ exercices/2.10/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './Components/App/App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {HomePage,CinemaPageRender,MovieListPageRender} from "./Components/App/App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/cinemapage",
+    element: <CinemaPageRender />,
+  },
+  {
+    path: "/movielist",
+    element: <MovieListPageRender />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>  
+  </React.StrictMode>
+);
