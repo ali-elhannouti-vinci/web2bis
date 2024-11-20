6c503f005a015e3176b3f496132108613f4f266d
ali-elhannouti-vinci <ali.elhannouti@student.vinci.be>
2024-11-21 00:09:00 +0100

    2.13

--- exercices/2.13/src/App.tsx
+++ exercices/2.13/src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Joke from "./type";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const defaultJoke: Joke = {
+  error: false,
+  category: "Undefined",
+  type: "single",
+  joke: "Undefined",
+  flags: {
+    nsfw: false,
+    religious: false,
+    political: false,
+    racist: false,
+    sexist: false,
+    explicit: false,
+  },
+  id: 0,
+  safe: true,
+  lang: "en",
+};
+
+
+  const [joke,setJoke] = useState<Joke>(defaultJoke);
+
+  useEffect(() => {
+    fetch("https://v2.jokeapi.dev/joke/Any?type=single").then(
+      (jokeRes) => {
+        if (!jokeRes.ok) 
+          throw new Error(
+            `fetch error : ${jokeRes.status} : ${jokeRes.statusText}`
+          );
+        return jokeRes.json();
+      }
+    ).then(
+      (joke) => {
+        setJoke(joke)
+      }
+    ).catch((e) => {
+      console.error(`App Error : `,e);
+    })
+  },[])
+
+  return <div>
+    <div>Category :
+      {joke.category}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>Joke : {joke.joke}</div>
+  </div>;
 }
-
-export default App
+export default App;
